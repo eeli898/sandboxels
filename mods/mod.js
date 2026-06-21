c0262b36a0d3c74edfa7c0beb55a562feb0297f0
eeli898 <elibrave333@gmail.com>
2026-06-21 15:05:38 -0400

    Add files via upload

---
+++ mods/mod.js
@@ -0,0 +1,10 @@
+elements.testing_element = {
+    color: "#49a8ab",
+    behavior: behaviors.WALL,
+    category: "testing",
+    state: "solid",
+    stateHigh: "carbon_dioxide",
+    tempHigh: 100,
+    tempLow: 0,
+    stateLow: "ice",
+}
